9e2281d956d8a25e878a0a595158a0e91e46c94b
haydenhatfield <haydenhatfield@hanovernorwichschools.org>
2016-05-27 14:53:16 -0400

    Updated from Brython Server: 5/27/2016 2:53:16 PM

--- platformer.py
+++ platformer.py
@@ -18,7 +18,7 @@ bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
 bg = Sprite(bg_asset, (0,0))
 
 RedS = ImageAsset("RedScaffolding.png")
-Sprite(RedS, (0, 300))
+Sprite(RedS, (0, 500))
 
 
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
