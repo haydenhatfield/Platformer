0ac8e5401d7a368f9dd020fbb3397e6cc7417dc9
haydenhatfield <haydenhatfield@hanovernorwichschools.org>
2016-06-06 10:19:09 -0700

    Updated from Brython Server: 6/6/2016 1:23:24 PM

--- platformer.py
+++ platformer.py
@@ -104,7 +104,7 @@ class Platformer(App):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.p = Player((200,200))
-        self.fl = Floor
+        self.fl = Floor((100,100))
         
     def step(self):
         self.p.step()
